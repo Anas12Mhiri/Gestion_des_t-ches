c328de9dec281ffd24748e20dde1314db94478d0
Anas Mhiri <144902181+Anas12Mhiri@users.noreply.github.com>
2025-12-07 22:30:57 +0100

    Fix task display logic in Main.py

--- Main.py
+++ Main.py
@@ -495,8 +495,8 @@ m=taskManager()
 
 #affiche les taches selon
 taches_afficher = st.selectbox("Selectioner la façon d'affichage", ["","Statut", "Priorite", "Date_echeance"])
-#data = m.afficher_taches(taches_afficher)
-st.write(data.loc[:,data.columns !="id"])
+data = m.afficher_taches(taches_afficher)
+#st.write(data.loc[:,data.columns !="id"])
 if taches_afficher:  # Check if something was selected
     data = m.afficher_taches(taches_afficher)
     st.write(data.loc[:, data.columns != "id"])
